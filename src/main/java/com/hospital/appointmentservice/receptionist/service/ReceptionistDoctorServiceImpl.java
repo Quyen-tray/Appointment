33a7d64fd1093000bc2cd25c44611209c7a14c11
LongNV2004 <longnguyen01042004@gmail.com>
2025-07-24 15:12:05 +0700

    update doctorlist for receptionist

---
+++ src/main/java/com/hospital/appointmentservice/receptionist/service/ReceptionistDoctorServiceImpl.java
@@ -0,0 +1,33 @@
+package com.hospital.appointmentservice.receptionist.service;
+
+
+import com.hospital.appointmentservice.admin.model.Doctor;
+import com.hospital.appointmentservice.doctor.repository.DoctorRepository;
+import com.hospital.appointmentservice.receptionist.dto.DoctorResponseDTO;
+import com.hospital.appointmentservice.receptionist.service.ReceptionistDoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ReceptionistDoctorServiceImpl implements ReceptionistDoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    @Override
+    public List<DoctorResponseDTO> getAllDoctors() {
+        List<Doctor> doctors = doctorRepository.findAll();
+        return doctors.stream().map(doc -> new DoctorResponseDTO(
+                doc.getId(),
+                doc.getFullName(),
+                doc.getStaff().getEmail(),
+                doc.getStaff().getPhone(),
+                doc.getSpecialization(),
+                doc.getStaff().getDepartment() != null ? doc.getStaff().getDepartment().getName() : "Chưa có",
+                doc.getStaff().getStatus()
+        )).collect(Collectors.toList());
+    }
+}
